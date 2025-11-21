15b3898af6f821dc9229df04452f69464bc40896
Elijah Lykins <elijahlykins@lykinsai.com>
2025-11-21 03:46:02 +0000

    File changes

--- src/components/ui/scroll-area.jsx
+++ src/components/ui/scroll-area.jsx
@@ -7,7 +7,8 @@ const ScrollArea = React.forwardRef(({ className, children, ...props }, ref) => 
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
-    {...props}>
+    {...props}
+  >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
@@ -29,8 +30,9 @@ const ScrollBar = React.forwardRef(({ className, orientation = "vertical", ...pr
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
     )}
-    {...props}>
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    {...props}
+  >
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border dark:bg-[#171515]" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
